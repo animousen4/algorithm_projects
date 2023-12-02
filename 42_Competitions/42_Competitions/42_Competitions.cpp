a43fea835d4b2be94cd69a1fff95a52b11292788
animousen4 <ilya_kozyro@mail.ru>
2023-12-02 12:57:32 +0300

    upd

--- 42_Competitions/42_Competitions/42_Competitions.cpp
+++ 42_Competitions/42_Competitions/42_Competitions.cpp
@@ -5,8 +5,16 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <math.h>
 using namespace std;
+int getMin(int* p, int from, int to, int*pBlocks, int &blockSize, int& blockAmount) {
+    int min = p[from];
+    for (int i = from + 1; i <= to; i++)
+        if (min > p[i])
+            min = p[i];
 
+    return min;
+}
 int main()
 {   
 
@@ -16,25 +24,67 @@ int main()
     ifstream inputFile("input.txt");
     inputFile >> n;
 
+    n = n + 1;
     participants = new int* [n];
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i < n; i++) {
         participants[i] = new int[3];
         inputFile >> participants[i][0] >> participants[i][1] >> participants[i][2];
     }
     inputFile.close();
 
-
-    sort(participants, participants + n, [](int* a, int* b) {
+    sort(participants + 1, participants + n, [](int* a, int* b) {
         return a[0] < b[0];
     });
 
-    for (int i = 0; i < n; i++)
-        cout << participants[i][0] << " ";
+    int* p = new int[n];
+    for (int i = 1; i < n; i++) {
+        p[i] = INT_MAX;
+    }
+    
+    int blockSize = sqrt(n - 1);
+    int blockAmount = ceil((n - 1) / (double)blockSize);
+    int* pBlocks = new int[blockAmount];
+
+    int c = n - 1;
+    for (int i = 1; i < n; i++) {
+        int min = getMin(p, 1, participants[i][1]);
+        if (min > participants[i][2]) {
+            int placeIn2 = participants[i][1];
+            int placeIn3 = participants[i][2];
+            p[placeIn2] = placeIn3;
+        }
+        else c--;
+        cout << min << endl;
+        for (int i = 1; i < n; i++)
+            cout << p[i] << " ";
+
+        cout << endl;
+    }
+
+    for (int i = 1; i < n; i++)
+        cout << p[i] << " ";
+
+    cout << endl;
+
+    cout << c;
 
     ofstream outputFile("output.txt");
 
+    outputFile << c;
     
     outputFile.close();
 
+    return 0;
+
 }
+
+/*
+5
+2 1 5
+1 2 4
+4 4 3
+3 5 1
+5 3 2
+
+*/
