6ab54adba483b138fbbf7a13ad4a14dfc98a9f7c
animousen4 <ilya_kozyro@mail.ru>
2023-12-02 21:26:40 +0300

    upd f

--- 42_Competitions/42_Competitions/42_Competitions.cpp
+++ 42_Competitions/42_Competitions/42_Competitions.cpp
@@ -36,9 +36,10 @@ struct BlockStruct {
         if (blockLeft == blockRight)
             min = getMinAtArray(p, l, r);
         else {
+
             int min1 = getMinAtArray(p, l, (blockLeft + 1) * blockSize);
             int min2 = getMinAtArray(pBlocks, (blockLeft + 1), blockRight);
-            int min3 = blockRight * blockSize == r ? INT_MAX : getMinAtArray(p, blockRight * blockSize, r);
+            int min3 = getMinAtArray(p, blockRight * blockSize, r);
 
             min = min1;
 
@@ -73,8 +74,8 @@ struct BlockStruct {
 
 private:
     int getMinAtArray(int* p, int l, int r) {
-        int min = p[l];
-        for (int i = l + 1; i < r; i++)
+        int min = INT_MAX;
+        for (int i = l; i < r; i++)
             if (p[i] < min)
                 min = p[i];
 
@@ -123,13 +124,10 @@ int main()
 
     int c = n;
     for (int i = 0; i < n; i++) {
-        //int min = getMin(p, 0, participants[i][1] + 1);
         int min = bs.getMin(0, participants[i][1] + 1);
         if (min > participants[i][2]) {
             int placeIn2 = participants[i][1];
             int placeIn3 = participants[i][2];
-
-            //p[placeIn2] = placeIn3;
             bs.modifyMin(placeIn2, placeIn3);
         }
         else 
