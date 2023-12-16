aed0f46294a77c3d024a07e1ee962fd0ff8923e3
animousen4 <ilya_kozyro@mail.ru>
2023-12-17 01:14:17 +0300

    upd

--- 18_Hackers/18_Hackers/18_Hackers.cpp
+++ 18_Hackers/18_Hackers/18_Hackers.cpp
@@ -6,18 +6,83 @@
 #include <vector>
 
 using namespace std;
+struct Mark {
+    int f = -1;
+    int s = -1;
+    bool dS = false;
+    bool notVisitedF() {
+        return f == -1;
+    }
+
+    bool notVisitedS() {
+        return s == -1;
+    }
+
+    bool isBlack() {
+        return s != -1;
+    }
+
+    bool isGray() {
+        return f != -1;
+    }
+
+    bool pushMark(int m) {
+        if (notVisitedF()) {
+            f = m;
+            return true;
+        }
+        else
+            if (notVisitedS()) {
+                s = m;
+                return true;
+            }
+        return false;
+    }
+
+    bool bothVisited() {
+        return !notVisitedF() && !notVisitedS();
+    }
+};
+
+Mark* marks;
+int globalMark = 1;
+void dfs1(int i, vector<vector<int>>& listSm) {
+
+    marks[i].f = globalMark;
+    globalMark++;
+
+    for (int j = 0; j < listSm[i].size(); j++)
+        if (!marks[listSm[i][j]].isGray())
+            dfs1(listSm[i][j], listSm);
+    marks[i].s = globalMark;
+    globalMark++;
+
+}
+
+void dfs2(int i, vector<vector<int>>& listSm, vector<int>& visited) {
+
+    marks[i].dS = true;
+    visited.push_back(i);
+
+    for (int j = 0; j < listSm[i].size(); j++)
+        if (!marks[listSm[i][j]].dS)
+            dfs2(listSm[i][j], listSm, visited);
+
+}
 int main()
 {
     int n;
-    int* terminal;
-    vector<int>* listSm;
+    
+    
 
 
     ifstream inputFile("input.txt");
 
     inputFile >> n;
-    terminal = new int[n];
-    listSm = new vector<int>[n];
+    vector<int> terminal(n);
+    vector<vector<int>> listSm(n);
+    vector<vector<int>> reverseListSm(n);
+    marks = new Mark[n];
     for (int i = 0; i < n; i++)
         inputFile >> terminal[i];
 
@@ -28,9 +93,43 @@ int main()
         if (a == 0 && b == 0)
             break;
         listSm[a - 1].push_back(b - 1);
+        reverseListSm[b - 1].push_back(a - 1);
         k++;
     }
 
+    inputFile.close();
+
+    /*bool haveCondensation = true;
+
+    while (haveCondensation) {
+        
+    }*/
+
+    for (int i = 0; i < n; i++)
+        if (!marks[i].isGray()) {
+            dfs1(i, listSm);
+        }
+
+    vector<int> vv;
+    for (int i = 0; i < n; i++)
+        if (!marks[i].dS) {
+            vv.clear();
+            dfs2(i, reverseListSm, vv);
+            for (auto versh : vv)
+                cout << versh + 1 << "\t";
+            cout << endl;
+        }
+   /* for (int i = 0; i < n; i++) {
+        cout << i + 1 << "\t";
+    }
+    cout << endl;
+    for (int i = 0; i < n; i++) {
+        cout << marks[i].f << "\t";
+    }
+    cout << endl;
+    for (int i = 0; i < n; i++) {
+        cout << marks[i].s << "\t";
+    }*/
     
         
 
